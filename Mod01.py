a7388755f5831222033bbe9342c4e876117c7549
Tommy H Yeargin Jr <yeargin2021@gmail.com>
2024-07-02 21:59:47 -0400

    Update

--- Mod01.py
+++ Mod01.py
@@ -1,21 +1,25 @@
-def main():
-    def top():
-        return """
-                <html>
-                    <head></head>
-                    <title>Hello world!</title>
-                    <body>
-                        <h1>hi there</h1>
-                """
+#def main():
+def top():
+    print ("""
+            <html>
+                <head></head>
+                <title>Hello world!</title>
+                <body>
+                    <h1>hi there</h1>
+            """)
+def body():
     file = open("blogPost01.txt", "r")
     for i in file:
         print(i)
     file.close()
-    return """
+def bottom():
+    print ("""
                 </body>
             </html>
-            """
+            """)
 
 
 if __name__ == '__main__':
-    print(main())
+    top()
+    body()
+    bottom()
